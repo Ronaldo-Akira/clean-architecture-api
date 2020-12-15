a604b235e583c27e3e9ef4a7d5e4372c37fc4b48
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2020-12-14 23:34:49 -0300

    test: validate emails with domain part larger than 63 chars

--- src/usecases/register_user_on_mailing_list/entities/email-validation.spec.ts
+++ src/usecases/register_user_on_mailing_list/entities/email-validation.spec.ts
@@ -36,6 +36,11 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeFalsy()
   })
 
+  test('should not accept domain with a part larger than 63 chars', () => {
+    const email = 'any@' + 'd'.repeat(64) + '.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
   test('should accept valid email', () => {
     const email = 'any@email.com'
     expect(Email.validate(email)).toBeTruthy()
