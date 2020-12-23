23058b74b213e6962944f4ec310ae11f6ab227df
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2020-12-23 00:19:19 -0300

    test: verify name while creating user

--- src/usecases/register_user_on_mailing_list/entities/user.spec.ts
+++ src/usecases/register_user_on_mailing_list/entities/user.spec.ts
@@ -1,6 +1,7 @@
 import { User } from './user'
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 
 describe('user domain class', () => {
   test('should not create user with invalid e-mail address', async () => {
@@ -8,4 +9,16 @@ describe('user domain class', () => {
     const error = User.create({ name: 'any_name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
   })
+})
+
+test('should not create user with invalid name (too few characters)', () => {
+  const invalidName = 'O      '
+  const error = User.create({ name: invalidName, email: 'any@mail.com' })
+  expect(error).toEqual(left(new InvalidNameError()))
+})
+
+test('should not create user with invalid name (too many characters)', () => {
+  const invalidName = 'O'.repeat(257)
+  const error = User.create({ name: invalidName, email: 'any@mail.com' })
+  expect(error).toEqual(left(new InvalidNameError()))
 })
