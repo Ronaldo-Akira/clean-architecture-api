283877a8302517abb2c43ef2a982f7ac80cc7df6
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2021-01-19 22:16:59 -0300

    feat: improve error classes

--- src/usecases/register_user_on_mailing_list/entities/user.ts
+++ src/usecases/register_user_on_mailing_list/entities/user.ts
@@ -6,8 +6,8 @@ import { Name } from './name'
 import { Email } from './email'
 
 export class User {
-  public readonly name: Name
   public readonly email: Email
+  public readonly name: Name
 
   private constructor (name: Name, email: Email) {
     this.name = name
@@ -17,12 +17,12 @@ export class User {
   static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     if (nameOrError.isLeft()) {
-      return left(new InvalidEmailError())
+      return left(new InvalidNameError(userData.name))
     }
 
     const emailOrError = Email.create(userData.email)
     if (emailOrError.isLeft()) {
-      return left(new InvalidEmailError())
+      return left(new InvalidEmailError(userData.email))
     }
 
     const name: Name = nameOrError.value as Name
