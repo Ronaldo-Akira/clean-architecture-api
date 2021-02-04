66d299501b4eb3e99eefb03cdfbaf61368668123
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2021-02-03 22:26:07 -0300

    feat: web controller should when user is created

---
+++ src/web-controllers/register-user-controller.ts
@@ -0,0 +1,21 @@
+import { UserData } from '@/entities'
+import { RegisterUserOnMailingList } from '@/usecases/register_user_on_mailing_list'
+import { HttpResponse, HttpRequest } from '@/web-controllers/ports'
+import { created } from '@/web-controllers/util'
+
+export class RegisterUserController {
+  private readonly usecase: RegisterUserOnMailingList
+
+  constructor (usecase: RegisterUserOnMailingList) {
+    this.usecase = usecase
+  }
+
+  public async handle (request: HttpRequest): Promise<HttpResponse> {
+    const userData: UserData = request.body
+    const response = await this.usecase.RegisterUserOnMailingList(userData)
+
+    if (response.isRight()) {
+      return created(response)
+    }
+  }
+}
